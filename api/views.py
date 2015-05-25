bbb4436e61d7c737e9dfc6bda49ced4980f5f986
Edinunzio <e.dinunzio@gmail.com>
2015-05-25 19:40:34 -0400

    documenting methods and updating readme

--- api/views.py
+++ api/views.py
@@ -2,6 +2,28 @@ from django.http import JsonResponse
 import json
 import urllib2
 
+"""
+Peloton Back End Test Task
+
+Web Service Background:
+    - Consumes 2 separate streams of increasing integers
+    - Can be queried to return the updated current element of the merged list
+    - Merged lists maintain increasing order
+
+App Requirements:
+    - Ability to query server with
+        http://localhost:8000/quiz/merge?stream1=<stream_name_1>&stream2=<stream_name_2>
+    - Communicate with Peloton server endpoint for consuming streams of data
+        https://api.pelotoncycle.com/quiz/next/<stream_name>
+
+Notes:
+    - Standard django 1.8 deployment
+    - No additional site-packages to install
+    - 8000 was chosen for the default port for a django project
+    - Repo located at https://github.com/Edinunzio/peloton
+
+"""
+
 
 def merge(request):
     """
